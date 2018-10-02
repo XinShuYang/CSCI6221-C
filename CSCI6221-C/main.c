5b25cef60b0945c3b40fa5eae0e0e36d020918f5
xsy <xsy@xsy.local>
2018-10-02 14:04:06 -0400

    Initial Commit

---
+++ CSCI6221-C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CSCI6221-C
+//
+//  Created by xsy on 10/2/18.
+//  Copyright © 2018 xsy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
